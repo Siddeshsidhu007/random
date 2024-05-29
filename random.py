c5377bdab4f2d0b89d6334ed80cdb3b03dba46ca
Siddeshsidhu007 <168621315+Siddeshsidhu007@users.noreply.github.com>
2024-05-29 14:53:24 +0530

    Add files via upload

---
+++ random.py
@@ -0,0 +1,28 @@
+import random
+
+def number_guessing_game():
+    print("Welcome to the Number Guessing Game!")
+    print("I'm thinking of a number between 1 and 100.")
+
+    secret_number = random.randint(1, 100)
+
+    attempts = 0
+    while True:
+        guess = int(input("Enter your guess: "))
+        attempts += 1
+
+        if guess < secret_number:
+            print("Too low! Try again.")
+        elif guess > secret_number:
+            print("Too high! Try again.")
+        else:
+            print(f"Congratulations! You guessed the number {secret_number} in {attempts} attempts.")
+            break
+
+    play_again = input("Do you want to play again? (yes/no): ").lower()
+    if play_again == 'yes':
+        number_guessing_game()
+    else:
+        print("Thank you for playing the Number Guessing Game!")
+
+number_guessing_game()
